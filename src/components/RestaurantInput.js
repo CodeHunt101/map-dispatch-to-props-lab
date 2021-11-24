659f8a9d5bb7d1abcce69c718304ba393fed9afd
Harold Torres Marino <haroldtm55@gmail.com>
2021-11-24 22:34:01 +1100

    commit tree 4fbae311a2b78037dcf429f5ce886cb9e6327384

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -49,6 +49,4 @@ export class RestaurantInput extends Component {
   }
 };
 
-
-//connect this component by wrapping RestaurantInput below
 export default connect(null, { addRestaurant })(RestaurantInput)
